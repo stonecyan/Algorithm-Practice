fc4f38eedee8b1bfe6d1dfa96c352542255a3520
Stone Yan <stoneyan1995@gmail.com>
2018-09-16 20:54:50 -0700

    FizzBuzz

---
+++ FizzBuzz/FizzBuzz/Program.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBUzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i;
+
+            for (i = 1; i <= 100; i++)
+            {
+                if (i % 5 == 0 && i % 3 == 0)
+                {
+                    Console.WriteLine("FizzBuzz");
+                }
+
+                else if (i % 5 == 0 && i % 3 != 3)
+                {
+                    Console.WriteLine("Buzz");
+                }
+
+                else if (i % 5 != 0 && i % 3 == 0)
+                {
+                    Console.WriteLine("Fizz");
+                }
+
+                else
+                {
+                    Console.WriteLine(i);
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
