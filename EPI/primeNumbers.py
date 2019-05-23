79392d16f4ba628754fd9eba70a0b94d371db62f
Stone Yan <40185228+stonecyan@users.noreply.github.com>
2019-05-23 14:13:14 -0400

    Update primeNumbers.py

--- EPI/primeNumbers.py
+++ EPI/primeNumbers.py
@@ -1,9 +1,18 @@
+    
 def primeNumbers(n):
     if n < 2:
         return []
     primes = [2]
-    size = (n - 3) // 2 + 1
-    i = 0
-    while i < n:
-        if i % 2 == 0:
-            i + +
+    isPrime = [True]*((n-3)//2+1)
+    i=3
+    while i<=n:
+      if isPrime[i//2-1]:
+        primes.append(i)
+      if i+i*2<n: 
+        for x in range(i*3, n, i*2):
+          isPrime[(x-1)//2-1]=False
+      i+=2
+    return primes
+
+x=primeNumbers(27)
+print(x)
